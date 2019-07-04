7c1a87783978691ec01ba862da826179616abf2f
Ronit Yaakovson <48907587+ronityako@users.noreply.github.com>
2019-07-04 11:37:09 +0300

    Add files via upload

---
+++ angular/ls5/homework/bankForm/src/app/bank-icon/bank-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bank-icon',
+  templateUrl: './bank-icon.component.html',
+  styleUrls: ['./bank-icon.component.css']
+})
+export class BankIconComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
